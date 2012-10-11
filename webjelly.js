829639d3128575c56010517f77187c2823fa1486
Steven Ruppert <blendmaster@gmail.com>
2012-10-11 14:29:57 -0600

    Fix more embarassing errors
    Flat shading works now. Gouraud shading actually works now, only there's
    something wrong about the normal calculation. Gotta fix later

--- webjelly.js
+++ webjelly.js
@@ -2,7 +2,7 @@
 original commented source there. */
 (function(){
   "use strict";
-  var canvas, width, height, k, ref$, v, shading, x$, fov, triangles, vertices, verticesBuffer, normalsBuffer, trianglesBuffer, staging, distance, rotation, currentRot, gouraud, flatNorms, flatVerts, flatIndices, calculateNormalsAndFlats, setupBuffers, resetStage, draw, parse, pointUnder, out$ = typeof exports != 'undefined' && exports || this;
+  var canvas, width, height, k, ref$, v, shading, x$, fov, flatLightDirection, flatProgram, gouraudProgram, triangles, vertices, verticesBuffer, normalsBuffer, trianglesBuffer, staging, distance, rotation, currentRot, gouraud, flatNorms, flatVerts, calculateNormalsAndFlats, setupBuffers, resetStage, draw, parse, pointUnder, out$ = typeof exports != 'undefined' && exports || this;
   canvas = document.getElementById('canvas');
   width = canvas.width, height = canvas.height;
   try {
@@ -24,6 +24,7 @@ original commented source there. */
   }
   x$.addEventListener('click', function(){
     shading = 'flat';
+    flatProgram();
     setupBuffers();
     draw();
   });
@@ -33,6 +34,7 @@ original commented source there. */
   }
   x$.addEventListener('click', function(){
     shading = 'gouraud';
+    gouraudProgram();
     setupBuffers();
     draw();
   });
@@ -66,40 +68,71 @@ original commented source there. */
   x$.enable(CULL_FACE);
   x$.clearColor(0, 0, 0, 1);
   x$.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
-  (function(){
-    var x$, vertexShader, fragmentShader;
-    x$ = vertexShader = this.createShader(VERTEX_SHADER);
-    this.shaderSource(x$, "attribute vec3 coord;\nattribute vec3 normal;\n\nvarying float NdotL;\n\nuniform mat4 ModelViewMatrix;\nuniform mat4 ProjectionMatrix;\nuniform mat3 NormalMatrix;\nuniform vec3 LightLocation;\n\nvoid main() {\n  vec4 WorldCoord = ModelViewMatrix * vec4(coord,1.0);   // convert to world coordinates\n  vec3 L = normalize(LightLocation - WorldCoord.xyz);    // L vector for illumination\n  vec3 WorldNormal = NormalMatrix * normal;                // normal in world coordinates\n  vec3 N = normalize(WorldNormal);                       // N vector for illumination\n  NdotL = dot(N,L);                                      // part of diffuse term (multiplied by k_d's etc in the fragment shader)\n  gl_Position = ProjectionMatrix * WorldCoord;             // gl_Position is a predefined variable\n    // a correctly written vertex shader should write screen space coordinates to gl_Position\n    // they are used on the rasterization stage!\n}");
-    this.compileShader(x$);
-    if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
-      throw new Error("couldn't compile vertex shader!");
-    }
-    x$ = fragmentShader = this.createShader(FRAGMENT_SHADER);
-    this.shaderSource(x$, "precision mediump float;\n\nvarying float NdotL;   // interpolated NdotL values (output of the vertex shader!)\n// note that interpolation qualifiers have to match between vertex and fragment shader\n\nuniform float LightIntensity;\nuniform float AmbientIntensity;\nuniform vec3 DiffuseAndAmbientCoefficient;     // for RGB: this is why it's a 3D vector\n\nvoid main() {\n  gl_FragColor = vec4(\n    (LightIntensity * (NdotL > 0.0 ? NdotL : 0.0) + AmbientIntensity) *\n    DiffuseAndAmbientCoefficient,\n    1);\n  // note that some simplifying assumptions are made in the above formula\n  //  for example, k_a=k_d is used; also, specular term is not used; also, no attenuation here\n}");
-    this.compileShader(x$);
-    if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
-      throw new Error("couldn't compile fragment shader!");
-    }
-    x$ = window.program = this.createProgram();
-    this.attachShader(x$, vertexShader);
-    this.attachShader(x$, fragmentShader);
-    this.linkProgram(x$);
-    if (!this.getProgramParameter(x$, LINK_STATUS)) {
-      throw new Error("couldn't intialize shader program!");
-    }
-    this.useProgram(x$);
-    this.uniform1f(this.getUniformLocation(program, 'LightIntensity'), 0.9);
-    this.uniform1f(this.getUniformLocation(program, 'AmbientIntensity'), 0.2);
-    this.uniform3f(this.getUniformLocation(program, 'DiffuseAndAmbientCoefficient'), 1, 1, 1);
-    this.uniform3fv(this.getUniformLocation(program, 'LightLocation'), [-1, -1, 0]);
-  }.call(gl));
+  flatLightDirection = vec3.normalize(vec3.direction([-1, -1, 0], [0, 0, 5]));
+  flatProgram = function(){
+    (function(){
+      var x$, vertexShader, fragmentShader;
+      x$ = vertexShader = this.createShader(VERTEX_SHADER);
+      this.shaderSource(x$, "attribute vec3 coord;\nattribute vec3 normal;\n\nvarying float NdotL;\n\nuniform mat4 ModelViewMatrix;\nuniform mat4 ProjectionMatrix;\nuniform mat3 NormalMatrix;\nuniform vec3 L;\n\nvoid main() {\n  vec4 WorldCoord = ModelViewMatrix * vec4(coord,1.0);   // convert to world coordinates\n  vec3 WorldNormal = NormalMatrix * normal;                // normal in world coordinates\n  vec3 N = normalize(WorldNormal);                       // N vector for illumination\n  NdotL = dot(N,L);                                      // part of diffuse term (multiplied by k_d's etc in the fragment shader)\n  gl_Position = ProjectionMatrix * WorldCoord;             // gl_Position is a predefined variable\n    // a correctly written vertex shader should write screen space coordinates to gl_Position\n    // they are used on the rasterization stage!\n}");
+      this.compileShader(x$);
+      if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
+        throw new Error("couldn't compile vertex shader!");
+      }
+      x$ = fragmentShader = this.createShader(FRAGMENT_SHADER);
+      this.shaderSource(x$, "precision mediump float;\n\nvarying float NdotL;   // interpolated NdotL values (output of the vertex shader!)\n\nuniform float LightIntensity;\nuniform float AmbientIntensity;\nuniform vec3 DiffuseAndAmbientCoefficient;     // for RGB: this is why it's a 3D vector\n\nvoid main() {\n  gl_FragColor = vec4(\n    (LightIntensity * (NdotL > 0.0 ? NdotL : 0.0) + AmbientIntensity) *\n    DiffuseAndAmbientCoefficient,\n    1);\n  // note that some simplifying assumptions are made in the above formula\n  //  for example, k_a=k_d is used; also, specular term is not used; also, no attenuation here\n}");
+      this.compileShader(x$);
+      if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
+        throw new Error("couldn't compile fragment shader!");
+      }
+      x$ = window.program = this.createProgram();
+      this.attachShader(x$, vertexShader);
+      this.attachShader(x$, fragmentShader);
+      this.linkProgram(x$);
+      if (!this.getProgramParameter(x$, LINK_STATUS)) {
+        throw new Error("couldn't intialize shader program!");
+      }
+      this.useProgram(x$);
+      this.uniform1f(this.getUniformLocation(program, 'LightIntensity'), 0.9);
+      this.uniform1f(this.getUniformLocation(program, 'AmbientIntensity'), 0.2);
+      this.uniform3f(this.getUniformLocation(program, 'DiffuseAndAmbientCoefficient'), 1, 1, 1);
+      this.uniform3fv(this.getUniformLocation(program, 'L'), flatLightDirection);
+    }.call(gl));
+  };
+  gouraudProgram = function(){
+    (function(){
+      var x$, vertexShader, fragmentShader;
+      x$ = vertexShader = this.createShader(VERTEX_SHADER);
+      this.shaderSource(x$, "attribute vec3 coord;\nattribute vec3 normal;\n\nvarying float NdotL;\n\nuniform mat4 ModelViewMatrix;\nuniform mat4 ProjectionMatrix;\nuniform mat3 NormalMatrix;\nuniform vec3 LightLocation;\n\nvoid main() {\n  vec4 WorldCoord = ModelViewMatrix * vec4(coord,1.0);   // convert to world coordinates\n  vec3 L = normalize(LightLocation - WorldCoord.xyz);    // L vector for illumination\n  vec3 WorldNormal = NormalMatrix * normal;                // normal in world coordinates\n  vec3 N = normalize(WorldNormal);                       // N vector for illumination\n  NdotL = dot(N,L);                                      // part of diffuse term (multiplied by k_d's etc in the fragment shader)\n  gl_Position = ProjectionMatrix * WorldCoord;             // gl_Position is a predefined variable\n    // a correctly written vertex shader should write screen space coordinates to gl_Position\n    // they are used on the rasterization stage!\n}");
+      this.compileShader(x$);
+      if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
+        throw new Error("couldn't compile vertex shader!");
+      }
+      x$ = fragmentShader = this.createShader(FRAGMENT_SHADER);
+      this.shaderSource(x$, "precision mediump float;\n\nvarying float NdotL;   // interpolated NdotL values (output of the vertex shader!)\n// note that interpolation qualifiers have to match between vertex and fragment shader\n\nuniform float LightIntensity;\nuniform float AmbientIntensity;\nuniform vec3 DiffuseAndAmbientCoefficient;     // for RGB: this is why it's a 3D vector\n\nvoid main() {\n  gl_FragColor = vec4(\n    (LightIntensity * (NdotL > 0.0 ? NdotL : 0.0) + AmbientIntensity) *\n    DiffuseAndAmbientCoefficient,\n    1);\n  // note that some simplifying assumptions are made in the above formula\n  //  for example, k_a=k_d is used; also, specular term is not used; also, no attenuation here\n}");
+      this.compileShader(x$);
+      if (!this.getShaderParameter(x$, COMPILE_STATUS)) {
+        throw new Error("couldn't compile fragment shader!");
+      }
+      x$ = window.program = this.createProgram();
+      this.attachShader(x$, vertexShader);
+      this.attachShader(x$, fragmentShader);
+      this.linkProgram(x$);
+      if (!this.getProgramParameter(x$, LINK_STATUS)) {
+        throw new Error("couldn't intialize shader program!");
+      }
+      this.useProgram(x$);
+      this.uniform1f(this.getUniformLocation(program, 'LightIntensity'), 0.9);
+      this.uniform1f(this.getUniformLocation(program, 'AmbientIntensity'), 0.2);
+      this.uniform3f(this.getUniformLocation(program, 'DiffuseAndAmbientCoefficient'), 1, 1, 1);
+      this.uniform3fv(this.getUniformLocation(program, 'LightLocation'), [-1, -1, -10]);
+    }.call(gl));
+  };
   calculateNormalsAndFlats = function(){
-    var k, j, i, to$, a, b, c, v0, v1, v2, cross, minz, miny, minx, maxz, maxy, maxx, x$, toCenter, _, toStage;
+    var j, i, to$, a, b, c, v0, v1, v2, cross, minz, miny, minx, maxz, maxy, maxx, x$, toCenter, _, toStage;
     gouraud = new Float32Array(vertices.length);
     flatNorms = new Float32Array(triangles.length * 3);
     flatVerts = new Float32Array(triangles.length * 3);
-    flatIndices = new Uint16Array(triangles.length);
-    j = k = 0;
+    j = 0;
     for (i = 0, to$ = triangles.length; i < to$; i += 3) {
       a = triangles[i] * 3;
       b = triangles[i + 1] * 3;
@@ -108,40 +141,21 @@ original commented source there. */
       v1 = Array.prototype.slice.call(vertices, b, 3 + b);
       v2 = Array.prototype.slice.call(vertices, c, 3 + c);
       cross = vec3.cross(vec3.direction(v0, v1, []), vec3.direction(v0, v2, []), []);
-      gouraud[a] += cross[0];
-      gouraud[a + 1] += cross[1];
-      gouraud[a + 2] += cross[2];
-      gouraud[b] += cross[0];
-      gouraud[b + 1] += cross[1];
-      gouraud[b + 2] += cross[2];
-      gouraud[c] += cross[0];
-      gouraud[c + 1] += cross[1];
-      gouraud[c + 2] += cross[2];
-      flatIndices[j] = j++;
-      flatIndices[j] = j++;
-      flatIndices[j] = j++;
-      flatVerts[k] = v0[0];
-      flatVerts[k + 1] = v0[1];
-      flatVerts[k + 2] = v0[2];
-      flatNorms[k] = cross[0];
-      flatNorms[k + 1] = cross[1];
-      flatNorms[k + 2] = cross[2];
-      k += 3;
-      flatVerts[k] = v1[0];
-      flatVerts[k + 1] = v1[1];
-      flatVerts[k + 2] = v1[2];
-      flatNorms[k] = cross[0];
-      flatNorms[k + 1] = cross[1];
-      flatNorms[k + 2] = cross[2];
-      k += 3;
-      flatVerts[k] = v2[0];
-      flatVerts[k + 1] = v2[1];
-      flatVerts[k + 2] = v2[2];
-      flatNorms[k] = cross[0];
-      flatNorms[k + 1] = cross[1];
-      flatNorms[k + 2] = cross[2];
-      k += 3;
+      gouraud[a] += cross[0], gouraud[a + 1] += cross[1], gouraud[a + 2] += cross[2];
+      gouraud[b] += cross[0], gouraud[b + 1] += cross[1], gouraud[b + 2] += cross[2];
+      gouraud[c] += cross[0], gouraud[c + 2] += cross[1], gouraud[c + 2] += cross[2];
+      flatVerts[j] = v0[0], flatVerts[j + 1] = v0[1], flatVerts[j + 2] = v0[2];
+      flatVerts[j + 3] = v1[0], flatVerts[j + 4] = v1[1], flatVerts[j + 5] = v1[2];
+      flatVerts[j + 6] = v2[0], flatVerts[j + 7] = v2[1], flatVerts[j + 8] = v2[2];
+      flatNorms[j] = cross[0], flatNorms[j + 1] = cross[1], flatNorms[j + 2] = cross[2];
+      flatNorms[j + 3] = cross[0], flatNorms[j + 4] = cross[1], flatNorms[j + 5] = cross[2];
+      flatNorms[j + 6] = cross[0], flatNorms[j + 7] = cross[1], flatNorms[j + 8] = cross[2];
+      j += 9;
     }
+    console.log(flatNorms);
+    console.log(flatVerts);
+    console.log(j);
+    console.log(triangles.length * 3);
     minx = miny = minz = Infinity;
     maxx = maxy = maxz = 0;
     for (i = 0, to$ = vertices.length; i < to$; i += 3) {
@@ -155,8 +169,6 @@ original commented source there. */
       minz <= x$ || (minz = x$);
       maxz >= x$ || (maxz = x$);
     }
-    log("min: " + minx + " " + miny + " " + minz);
-    log("max: " + maxx + " " + maxy + " " + maxz);
     toCenter = [-((minx + maxx) / 2), -((miny + maxy) / 2), -((minz + maxz) / 2)];
     toStage = (_ = Math.max(maxx - minx, maxz - miny, maxz - minz), _ = 2 / _, [_, _, _]);
     staging = mat4.identity();
@@ -165,6 +177,7 @@ original commented source there. */
   };
   setupBuffers = function(){
     var x$, y$;
+    console.log(shading);
     x$ = verticesBuffer = gl.createBuffer();
     gl.bindBuffer(ARRAY_BUFFER, x$);
     gl.bufferData(ARRAY_BUFFER, shading === 'flat' ? flatVerts : vertices, STATIC_DRAW);
@@ -172,15 +185,14 @@ original commented source there. */
     gl.enableVertexAttribArray(y$);
     gl.vertexAttribPointer(y$, 3, FLOAT, false, 0, 0);
     x$ = normalsBuffer = gl.createBuffer();
-    gl.bindBuffer(ELEMENT_ARRAY_BUFFER, x$);
-    gl.bufferData(ELEMENT_ARRAY_BUFFER, shading === 'flat' ? flatNorms : gouraud, STATIC_DRAW);
+    gl.bindBuffer(ARRAY_BUFFER, x$);
+    gl.bufferData(ARRAY_BUFFER, shading === 'flat' ? flatNorms : gouraud, STATIC_DRAW);
     y$ = gl.getAttribLocation(program, 'normal');
     gl.enableVertexAttribArray(y$);
     gl.vertexAttribPointer(y$, 3, FLOAT, false, 0, 0);
-    console.log(triangles);
     x$ = trianglesBuffer = gl.createBuffer();
     gl.bindBuffer(ELEMENT_ARRAY_BUFFER, x$);
-    gl.bufferData(ELEMENT_ARRAY_BUFFER, shading === 'flat' ? flatIndices : triangles, STATIC_DRAW);
+    gl.bufferData(ELEMENT_ARRAY_BUFFER, triangles, STATIC_DRAW);
   };
   resetStage = function(){
     rotation = mat4.identity();
@@ -199,8 +211,13 @@ original commented source there. */
     mat4.multiply(modelView, rot);
     mat4.multiply(modelView, staging);
     gl.uniformMatrix4fv(gl.getUniformLocation(program, 'ModelViewMatrix'), false, modelView);
-    gl.bindBuffer(ELEMENT_ARRAY_BUFFER, trianglesBuffer);
-    gl.drawElements(TRIANGLES, triangles.length, UNSIGNED_SHORT, 0);
+    if (shading === 'gouraud') {
+      gl.bindBuffer(ELEMENT_ARRAY_BUFFER, trianglesBuffer);
+      gl.drawElements(TRIANGLES, triangles.length, UNSIGNED_SHORT, 0);
+    } else {
+      gl.bindBuffer(ARRAY_BUFFER, verticesBuffer);
+      gl.drawArrays(TRIANGLES, 0, triangles.length);
+    }
   };
   parse = function(){
     var that, x$;
@@ -213,6 +230,11 @@ original commented source there. */
         ref$ = tokens.splice(0, 2), numTriangles = ref$[0], numVertices = ref$[1];
         triangles = new Uint16Array(tokens.splice(0, numTriangles * 3));
         vertices = new Float32Array(tokens.splice(0, numVertices * 3));
+        if (shading === 'flat') {
+          flatProgram();
+        } else {
+          gouraudProgram();
+        }
         calculateNormalsAndFlats();
         setupBuffers();
         resetStage();
@@ -239,6 +261,9 @@ original commented source there. */
   x$.addEventListener('mousedown', function(arg$){
     var i0, j0, p, rotate, stop;
     i0 = arg$.clientX, j0 = arg$.clientY;
+    if (currentRot == null) {
+      return;
+    }
     x$.style.cursor = 'move';
     p = pointUnder(i0, j0);
     rotate = function(arg$){
